9a90eee62f408bb81034dadd5efb8b0f0fd301ce
Nadejda Klinskaya <npantelyeva@gmail.com>
2023-08-04 01:46:47 +0300

    feat: add solution for reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  const arr = Array.from(String(n));
+  arr[0] === '-' ? arr.shift() : arr;
+  let arrNum = arr.map(i => Number(i));
+  console.log(arrNum);
+  const newArr = arrNum.reverse();
+  n = +newArr.join('');
+  return n;
 }
